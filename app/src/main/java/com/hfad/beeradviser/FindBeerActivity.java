cc859f747de6c5bac62d5375d1521660bebc42ef
Peter James Babiera <peter.babiera@safesat.com.ph>
2017-12-05 22:21:08 +0800

    Add display beertype on click

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -1,13 +1,33 @@
 package com.hfad.beeradviser;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+
+    }
+
+    public void onClickFindBeer(View view) {
+
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        brands.setText(beerType);
     }
 }
